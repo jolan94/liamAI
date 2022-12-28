6312bbc5e2254d778bdf699a003c6ffb39ec7561
Joe Jeyaseelan <jolan94@gmail.com>
2022-12-28 22:57:42 +0530

    api key changes

--- server/server.js
+++ server/server.js
@@ -29,9 +29,9 @@ app.post('/', async (req, res) => {
             model: "code-davinci-002",
             prompt: `${prompt}`,
             temperature: 0,
-            max_tokens: 100,
+            max_tokens: 5000,
             top_p: 1.0,
-            frequency_penalty: 0,
+            frequency_penalty: 0.5,
             presence_penalty: 0,
           });
 
