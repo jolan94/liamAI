1ea373078c75fc7e1495bff03c1d69c8d9f46ad3
Joe Jeyaseelan <jolan94@gmail.com>
2023-02-14 15:49:54 +0530

    changes fixes

--- server/server.js
+++ server/server.js
@@ -11,8 +11,9 @@ console.log("Above you can find server key")
 dotenv.config()
 
 const configuration = new Configuration({
-//   apiKey: process.env.OPENAI_API_KEY,
-apiKey: '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',
+  apiKey: process.env.OPENAI_API_KEY,
+// apiKey: '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',
+// apiKey: "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",
 });
 
 const openai = new OpenAIApi(configuration);
@@ -29,26 +30,21 @@ app.get('/', async (req, res) => {
 
 app.post('/', async (req, res) => {
     try {
-        const prompt = req.body.prompt;
-        console.log(`Input prompt here is ${prompt}`)
-
+        const promptGet = req.body.prompt;
+        console.log(`Input prompt here is ${promptGet}`)
           const response = await openai.createCompletion({
             model: "text-davinci-003",
-            prompt: `${prompt}`,
+            prompt: `${promptGet}`,
             temperature: 0,
             max_tokens: 3000,
             top_p: 1.0,
             frequency_penalty: 0.5,
             presence_penalty: 0,
           });
-
-          console.log(`Received data is here is ${response.data.choices[0]}`)
-
+          console.log(`Received data is here is ${response.data.choices[0].text}`)
           res.status(200).send({
-            prompt: response.data.choices[0].text
+            bot: response.data.choices[0].text
           });
-
-         
     } catch (error) {
         console.log(error);
         res.status(500).send(error || 'Something went wrong');
